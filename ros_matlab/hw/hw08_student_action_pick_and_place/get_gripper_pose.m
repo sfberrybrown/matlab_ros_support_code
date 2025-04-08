1bf3b6d1aa1863a59e08e888e89f2f9464cb14d3
Dr. Juan Rojas <rojas70@gmail.com>
2025-04-08 14:45:40 -0500

    Cleaned dictionary calls and fixed call to tf_listening time.

---
+++ ros_matlab/hw/hw08_student_action_pick_and_place/get_gripper_pose.m
@@ -0,0 +1,49 @@
+function mat_R_T_G = get_gripper_pose(optns,fing_alignment)
+% Return the gripper pose. 
+% Since we are computing the pose wrt to tool0, we do not have information
+% of the fingers. The 'fing_alignment' will tell whether the fingers are
+% aligned about the x-axis of the y-axis wrt matlab_coordinates for the
+% base_link. By default x-axis.
+
+    %% Local variables
+    if nargin == 1
+        fing_alignment = 'x';
+    end
+
+    % Indicates matlab's base_link does not match ros. Need adjustment.
+    frameAdjustmentFlag = optns{'frameAdjustmentFlag'};    
+    
+    % Indicates we have fingers but have not adjusted IKs for it.    
+    toolAdjustmentFlag  = optns{'toolAdjustmentFlag'}; 
+    
+    % List to tftree
+    r = optns{'rHandle'};
+    tf_listening_time   = r.tf_listening_time;    
+    
+    % Get gripper transform
+    tftree       = rostf('DataFormat','struct');     
+    base         = 'base_link';
+    end_effector = 'tool0';                         % When finger is properly modeled use 'gripper_tip_link'
+        
+    % Get end-effector pose wrt to base via getTransform(tftree,targetframe,sourceframe), where sourceframe is the reference frame 
+    % However, that order of parameters does not give the right answer. Need to reverse them!!!
+    try waitForTransform(tftree,'base_link','tool0',5);
+        current_pose = getTransform(tftree,base,end_effector,rostime('now'),'Timeout', optns('tf_listening_time'));
+    catch
+        % Try again
+        current_pose = getTransform(tftree,base,end_effector,rostime('now'),'Timeout', tf_listening_time);
+    end  
+        
+    % Convert gripper pose to matlab format
+    mat_R_T_G = ros2matlabPose(current_pose,frameAdjustmentFlag,toolAdjustmentFlag); 
+
+    %% Adjust orientation of gripper
+    if strcmpi(fing_alignment,'x')
+        ori = eul2tform([0,0,pi]);  
+    % 'y'
+    else
+        ori = eul2tform([-pi/2, pi, 0]);
+    end
+
+    mat_R_T_G(1:3,1:3) = ori(1:3,1:3);
+end
